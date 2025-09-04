69e96c6afef2103a27d183a832412da7572664f3
Cristi104 <petrecristian2004@gmail.com>
2025-09-04 15:27:14 +0300

    basic file browser

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <asm-generic/ioctls.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <termios.h>
+#include <dirent.h>
+
 
 struct termios original_attributes;
 
@@ -19,21 +22,76 @@ void enable_raw_mode(){
   raw.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_lflag &= ~(ECHO | ICANON | ECHONL | ISIG | IEXTEN);
+  raw.c_cflag &= ~(CSIZE | PARENB);
   raw.c_cflag |= CS8;
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+struct dirent* select_file(){
+  write(STDOUT_FILENO, "\033[2J\033[3J\033[H", 12);
+  struct dirent** filelist;
+  struct dirent* file;
+  int n;
+  n = scandir(".", &filelist, NULL, alphasort);
+  for(int i = 0; i < n; i++){
+    file = filelist[i];
+    write(STDOUT_FILENO, file->d_name, strlen(file->d_name));
+    write(STDOUT_FILENO, "\r\n", 2);
+  }
+  write(STDOUT_FILENO, "\033[H", 4);
+
+  char input[16];
+  memset(input, 0, 16);
+  while (read(STDIN_FILENO, &input, 15) >= 1 && strcmp(input, "q") != 0) {
+    if(strcmp(input, "\033[A") == 0){
+      write(STDOUT_FILENO, "\033[1A", 5);
+      memset(input, 0, 16);
+      continue;
+    }
+
+    if(strcmp(input, "\033[B") == 0){
+      write(STDOUT_FILENO, "\033[1B", 5);
+      memset(input, 0, 16);
+      continue;
+    }
+
+    if(strcmp(input, "\r") == 0){
+      write(STDOUT_FILENO, "\033[6n", 5);
+      int line, collumn;
+      scanf("\033[%u;%uR",&line, &collumn);
+      if(line - 1 < n){
+        file = malloc(sizeof(struct dirent)); 
+        *file = *filelist[line - 1];
+        free(filelist);
+        write(STDOUT_FILENO, "\033[2J\033[3J\033[H", 12);
+        return file;
+      }
+      memset(input, 0, 16);
+      continue;
+    }
+  }
+  write(STDOUT_FILENO, "\033[2J\033[3J\033[H", 12);
+  free(filelist);
+  return NULL;
 }
 
 int main(int argc, char *argv[])
 {
   enable_raw_mode();
+  struct dirent* file = select_file();
+  write(STDOUT_FILENO, file->d_name, strlen(file->d_name));
+  write(STDOUT_FILENO, "\r\n", 2);
 
-  // struct winsize w;
-  // ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-  // printf("%u, %u\n", w.ws_row, w.ws_col);
-  char input;
-  while (read(STDIN_FILENO, &input, 1) == 1 && input != 'q') {
-    printf("Read: %u\r\n", input);
+  char input[16];
+  memset(input, 0, 16);
+  while (read(STDIN_FILENO, &input, 15) >= 1 && strcmp(input, "q") != 0) {
+    if(strcmp(input, "a") == 0){
+      write(STDOUT_FILENO, "\033[2J\033[3J\033[H", 12);
+      memset(input, 0, 16);
+      continue;
+    }
+    write(STDOUT_FILENO, input, 15);
+    memset(input, 0, 16);
   }
-
   return EXIT_SUCCESS;
 }
